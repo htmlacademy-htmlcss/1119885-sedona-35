4fa0d3ba21948b467caa1fcd4c6ffaf1bbed758b
Sanyok133 <kozyuckov.alex@gmail.com>
2022-11-05 14:04:57 +0400

    Делает рабочими кнопки + и - в модальном окне

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ var search = document.querySelector(".search-button");
 var modal = document.querySelector(".modal");
 var scrollbar = document.querySelector("body");
 var closer = document.querySelector(".modal-close-button");
+var minus1 = document.querySelector(".minus-button-first");
+var plus1 = document.querySelector(".plus-button-first");
+var minus2 = document.querySelector(".minus-button-second");
+var plus2 = document.querySelector(".plus-button-second");
+var adult = document.querySelector("#adult");
+var adult = document.querySelector("#child");
 
 search.removeAttribute("href");
 
@@ -13,4 +19,20 @@ search.onclick = function() {
 closer.onclick = function() {
   modal.classList.add("modal-close");
   scrollbar.classList.remove("modal-open");
+}
+
+minus1.onclick = function() {
+  adult.value -= 1;
+}
+
+plus1.onclick = function() {
+  adult.value -= -1;
+}
+
+minus2.onclick = function() {
+  adult.value -= 1;
+}
+
+plus2.onclick = function() {
+  adult.value -= -1;
 }
